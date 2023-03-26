f1b8c0844c3b5be3e738b8bc58046c3b36b69bd8
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2023-03-25 23:19:32 -0400

    Finish document validation
    Next:
    - Implement API (see: https://earthstar-project.org/docs/developer-guide)
    - Implement CLI (see: https://github.com/earthstar-project/earthstar-cli)

--- src/document.rs
+++ src/document.rs
@@ -1,7 +1,14 @@
-use crate::address::Identity;
+use crate::{
+    address::{Identity, ShareAddress},
+    error::DocumentError,
+};
 use data_encoding::BASE32_NOPAD;
+use ed25519_dalek::{ed25519::signature::Signature, Signer};
 use sha2::{Digest, Sha256};
-use std::time::{Instant, SystemTime, UNIX_EPOCH};
+use std::{
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 pub struct Document {
     pub author: Identity,
@@ -11,17 +18,102 @@ pub struct Document {
     pub path: String,
     pub signature: String,
     pub timestamp: SystemTime,
-    pub share: String,
+    pub share: ShareAddress,
     pub share_signature: String,
-    pub delete_after: Option<Instant>,
+    pub delete_after: Option<SystemTime>,
     pub attachment_size: Option<i32>,
     pub attachment_hash: Option<String>,
 }
 
 impl Document {
-    pub fn hash_text(&self) -> String {
+    pub fn new(
+        author: Identity,
+        text: String,
+        text_hash: Option<String>,
+        format: String,
+        path: String,
+        signature: String,
+        timestamp: SystemTime,
+        share: ShareAddress,
+        share_signature: String,
+        delete_after: Option<SystemTime>,
+        attachment_size: Option<i32>,
+        attachment_hash: Option<String>,
+    ) -> Result<Self, DocumentError> {
+        let document = Self {
+            author,
+            text: text.clone(),
+            text_hash: text_hash.unwrap_or(Document::hash_text(text)),
+            format,
+            path,
+            signature,
+            timestamp,
+            share,
+            share_signature,
+            delete_after,
+            attachment_size,
+            attachment_hash,
+        };
+
+        if !document.validate_text() {
+            return Err(DocumentError::InvalidText);
+        }
+
+        if !document.validate_text_hash() {
+            return Err(DocumentError::InvalidTextHash);
+        }
+
+        if !document.validate_format() {
+            return Err(DocumentError::InvalidFormat);
+        }
+
+        if !document.validate_path() {
+            return Err(DocumentError::InvalidPath);
+        }
+
+        if !document.validate_signature() {
+            return Err(DocumentError::InvalidSignature);
+        }
+
+        if !document.validate_timestamp() {
+            return Err(DocumentError::InvalidTimestamp);
+        }
+
+        if !document.validate_share_signature() {
+            return Err(DocumentError::InvalidShareSignature);
+        }
+
+        if !document.validate_delete_after() {
+            return Err(DocumentError::InvalidDeleteAfter);
+        }
+
+        if !document.validate_attachment_size() {
+            return Err(DocumentError::InvalidAttachmentSize);
+        }
+
+        if !document.validate_attachment_hash() {
+            return Err(DocumentError::InvalidAttachmentHash);
+        }
+
+        Ok(document)
+    }
+
+    pub fn hash_document(&self) -> String {
         let mut hasher = Sha256::new();
-        hasher.update(self.text.as_bytes());
+        if self.attachment_hash.is_some() && self.attachment_size.is_some() {
+            hasher.update(format!(
+                "attachment_hash\t{}\nattachment_size\t{}\n",
+                self.attachment_hash.as_ref().unwrap_or(&String::default()),
+                self.attachment_size.as_ref().unwrap_or(&i32::default())
+            ));
+        }
+        hasher.update(format!("author\t{}\ndelete_after\t{}\nformat\t{}\npath\t{}\nshare\t{}\nshare_signature\t{}\ntext_hash\t{}\ntimestamp\t{}\n", self.author, self.delete_after_as_u128(), self.format, self.path, self.share, self.share_signature, self.text_hash, self.timestamp_as_u128()));
+        format!("b{}", BASE32_NOPAD.encode(&hasher.finalize()))
+    }
+
+    pub fn hash_text(text: String) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(text.as_bytes());
         format!("b{}", BASE32_NOPAD.encode(&hasher.finalize()))
     }
 
@@ -32,15 +124,135 @@ impl Document {
             .as_micros()
     }
 
+    pub fn delete_after_as_u128(&self) -> u128 {
+        self.delete_after
+            .map(|delete_after| {
+                delete_after
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap_or_default()
+                    .as_micros()
+            })
+            .unwrap_or_default()
+    }
+
+    pub fn validate_text(&self) -> bool {
+        self.text.as_bytes().len() <= 8000
+            && (self.attachment_hash.is_some()
+                && self.attachment_size.is_some()
+                && !self.text.is_empty()
+                || self.attachment_hash.is_none()
+                    && self.attachment_size.is_none()
+                    && self.text.is_empty())
+    }
+
     pub fn validate_text_hash(&self) -> bool {
         self.text_hash.chars().nth(0).unwrap_or_default() == 'b'
             && self.text_hash.len() == 53
             && BASE32_NOPAD.decode(self.text_hash[1..].as_bytes()).is_ok()
-            && self.text_hash == self.hash_text()
+            && self.text_hash == Document::hash_text(self.text.clone())
     }
 
     pub fn validate_timestamp(&self) -> bool {
         let timestamp_int = self.timestamp_as_u128();
-        timestamp_int > (10 as u128).pow(13) && timestamp_int < (2 as u128).pow(53) - 2
+        timestamp_int >= (10 as u128).pow(13) && timestamp_int <= (2 as u128).pow(53) - 2
+    }
+
+    pub fn validate_delete_after(&self) -> bool {
+        self.delete_after
+            .map(|delete_after| {
+                let delete_after_int = delete_after
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap_or_default()
+                    .as_micros();
+                delete_after_int >= (10 as u128).pow(13)
+                    && delete_after_int <= (2 as u128).pow(53) - 2
+                    && delete_after > self.timestamp
+            })
+            .unwrap_or_default()
+    }
+
+    pub fn validate_format(&self) -> bool {
+        self.format.is_ascii()
+            && !self.format.contains(|c: char| c.is_ascii_whitespace())
+            && !self.format.contains(|c: char| c.is_ascii_control())
+    }
+
+    pub fn validate_path(&self) -> bool {
+        self.path.is_ascii()
+            && !self.path.contains(|c: char| c.is_ascii_whitespace())
+            && !self.path.contains(|c: char| c.is_ascii_control())
+            && self.path.len() >= 2
+            && self.path.len() <= 512
+            && self.path.chars().nth(0).unwrap_or_default() == '/'
+            && &self.path[0..2] != "/@"
+            && !self.path.contains("//")
+            && ((self.delete_after.is_none() && !self.path.contains("!"))
+                || (self.delete_after.is_some() && self.path.contains("!")))
+            && !self.path.contains("?")
+            && !self.path.contains("#")
+            && !self.path.contains(";")
+            && !self.path.contains("<")
+            && !self.path.contains(">")
+            && !self.path.contains("\"")
+            && !self.path.contains("[")
+            && !self.path.contains("\\")
+            && !self.path.contains("]")
+            && !self.path.contains("^")
+            && !self.path.contains("{")
+            && !self.path.contains("|")
+            && !self.path.contains("}")
+            && (self.attachment_hash.is_some()
+                && self.attachment_size.is_some()
+                && Path::new(&self.path).extension().is_some()
+                || self.attachment_hash.is_none()
+                    && self.attachment_size.is_none()
+                    && Path::new(&self.path).extension().is_none())
+            && (!self.path.contains("~")
+                || self
+                    .path
+                    .contains(&("~@".to_owned() + &self.author.shortname)))
+    }
+
+    pub fn validate_signature(&self) -> bool {
+        self.signature.chars().nth(0).unwrap_or_default() == 'b'
+            && BASE32_NOPAD.decode(self.signature[1..].as_bytes()).is_ok()
+            && ((self.format == "es.5" && self.signature.len() == 104) || self.format != "es.5")
+            && self.signature
+                == format!(
+                    "b{}",
+                    BASE32_NOPAD.encode(
+                        &self
+                            .author
+                            .keypair
+                            .sign(&self.hash_document().as_bytes())
+                            .as_bytes()
+                    )
+                )
+    }
+
+    pub fn validate_share_signature(&self) -> bool {
+        self.share_signature.chars().nth(0).unwrap_or_default() == 'b'
+            && BASE32_NOPAD
+                .decode(self.share_signature[1..].as_bytes())
+                .is_ok()
+            && ((self.format == "es.5" && self.share_signature.len() == 104)
+                || self.format != "es.5")
+    }
+
+    pub fn validate_attachment_size(&self) -> bool {
+        self.attachment_size
+            .map(|attachment_size| attachment_size >= 0 && attachment_size <= 2_i32.pow(53) - 2)
+            .unwrap_or_default()
+    }
+
+    pub fn validate_attachment_hash(&self) -> bool {
+        self.attachment_hash
+            .as_ref()
+            .map(|attachment_hash| {
+                attachment_hash.chars().nth(0).unwrap_or_default() == 'b'
+                    && attachment_hash.len() == 53
+                    && BASE32_NOPAD.decode(attachment_hash[1..].as_bytes()).is_ok()
+            })
+            .unwrap_or_default()
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -26,3 +26,37 @@ pub enum IdentityError {
     #[diagnostic(code(identity::name::starts_with_digit))]
     StartsWithDigit,
 }
+
+#[derive(Error, Diagnostic, Debug)]
+pub enum DocumentError {
+    #[error("Document text must be less than or equal to 8 000 bytes, and cannot be empty if an attachment is present.")]
+    #[diagnostic(code(document::invalid_text))]
+    InvalidText,
+    #[error("Document text hash must be a valid SHA-256 hash of the document text, encoded in Base32 (RFC 4648, no padding) with a leading `b'.")]
+    #[diagnostic(code(document::invalid_text_hash))]
+    InvalidTextHash,
+    #[error("Document format must be a string of printable ASCII characters.")]
+    #[diagnostic(code(document::invalid_format))]
+    InvalidFormat,
+    #[error("Document path must be:\n- a string of printable ASCII characters\n- a string of valid URL path characters (ie, cannot include any of the following characters, `<>\"[\\]^`{{|}}')\n- cannot include `#', `?', or `;'\n- can only contain a file extension if an attachment is present\n- is a path that the document author is allowed to write to.")]
+    #[diagnostic(code(document::invalid_path))]
+    InvalidPath,
+    #[error("Document signature must be a valid Ed25519 signature of the document, encoded in Base32 (RFC 4648, no padding) with a leading `b', with a total length of 104 characters (in `es.5' format).")]
+    #[diagnostic(code(document::invalid_signature))]
+    InvalidSignature,
+    #[error("Document timestamp must be a valid Unix timestamp in microseconds, between 10^13 and 2^53 - 2, inclusive.")]
+    #[diagnostic(code(document::invalid_timestamp))]
+    InvalidTimestamp,
+    #[error("Document share signature must be encoded in Base32 (RFC 4648, no padding) with a leading `b', with a total length of 104 characters (in `es.5' format).")]
+    #[diagnostic(code(document::invalid_share_signature))]
+    InvalidShareSignature,
+    #[error("Document `delete_after' timestamp must be a valid Unix timestamp in microseconds, between 10^13 and 2^53 - 2, inclusive, and must be strictly greater than the document timestamp.")]
+    #[diagnostic(code(document::invalid_delete_after))]
+    InvalidDeleteAfter,
+    #[error("Document attachment size must be an integer between 0 and 2^53 - 2, inclusive.")]
+    #[diagnostic(code(document::invalid_attachment_size))]
+    InvalidAttachmentSize,
+    #[error("Document attachment hash must be a valid SHA-256 hash of the attachment, encoded in Base32 (RFC 4648, no padding) with a leading `b', with a total length of 53 characters.")]
+    #[diagnostic(code(document::invalid_attachment_hash))]
+    InvalidAttachmentHash,
+}
